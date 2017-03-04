a209c78aca15c8884f9e4c2bd6ea14613aea19b6
bakape <scorpid33@gmail.com>
2017-03-05 00:11:36 +0200

    Add change log

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+Project uses [Semantic Versioning](http://semver.org/)
+
+##vNext
+* Fix
+	- No commas in file JSON arrays
--- scripts/release.sh
+++ scripts/release.sh
@@ -5,8 +5,8 @@
 
 version=$1
 
-#sed -i "s/##vNext/##${version} - $(date +%Y-%m-%d)/" CHANGELOG.md
-#git commit CHANGELOG.md README.md -m $version || exit 1
+sed -i "s/##vNext/##${version} - $(date +%Y-%m-%d)/" CHANGELOG.md
+git commit CHANGELOG.md README.md -m $version || exit 1
 git tag $version || exit 1
 git push || exit 1
 git push origin $version || exit 1
