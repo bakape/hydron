701b0844ddb8c970c9fbd97d06aebafc9278b4a7
bakape <scorpid33@gmail.com>
2018-12-20 23:36:57 +0200

    Remove dead code

--- util/util.go
+++ util/util.go
@@ -16,44 +16,6 @@ func IsFetchable(url string) bool {
 	return false
 }
 
-// User passed an invalid file ID
-type InvalidIDError string
-
-func (e InvalidIDError) Error() string {
-	return "invalid id: " + string(e)
-}
-
-type sha1Sorter [][20]byte
-
-func (s sha1Sorter) Len() int {
-	return len(s)
-}
-
-func (s sha1Sorter) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-func (s sha1Sorter) Less(i, j int) bool {
-	a, b := s[i], s[j]
-	for i := range a {
-		switch {
-		case a[i] < b[i]:
-			return true
-		case a[i] > b[i]:
-			return false
-		}
-	}
-	return true
-}
-
-// Extract a copy of the underlying SHA1 key from a BoltDB key
-func ExtractKey(k []byte) (sha1 [20]byte) {
-	for i := range sha1 {
-		sha1[i] = k[i]
-	}
-	return
-}
-
 // Waterfall executes a slice of functions until the first error returned. This
 // error, if any, is returned to the caller.
 func Waterfall(fns ...func() error) (err error) {
